ef8f6df1fab1c5ae7605eacc7d463de194723519
India Evans <indiaaeevans@gmail.com>
2019-09-10 12:15:02 -0400

    remove localhost from urls

--- public/javascripts/getPhotos.js
+++ public/javascripts/getPhotos.js
@@ -4,137 +4,147 @@ const numbersRegEx = /^[0-9]+$/;
 // in case the document is already rendered
 if (document.readyState != 'loading') runScript();
 // modern browsers
-else if (document.addEventListener) document.addEventListener('DOMContentLoaded', runScript);
+else if (document.addEventListener)
+  document.addEventListener('DOMContentLoaded', runScript);
 // IE <= 8
-else document.attachEvent('onreadystatechange', function () {
+else
+  document.attachEvent('onreadystatechange', function() {
     if (document.readyState == 'complete') runScript();
-});
+  });
 
 function runScript() {
-    document.getElementById('form').addEventListener('submit', () => {
-        event.preventDefault();
-        const imageAreaEl = document.getElementById("imageArea");
-        const message = document.getElementById('messageInput').value;
-        clearChildElements(imageAreaEl);
-        printMessage(message, imageAreaEl);
-    });
+  document.getElementById('form').addEventListener('submit', () => {
+    event.preventDefault();
+    const imageAreaEl = document.getElementById('imageArea');
+    const message = document.getElementById('messageInput').value;
+    clearChildElements(imageAreaEl);
+    printMessage(message, imageAreaEl);
+  });
 }
 
 function clearChildElements(parent) {
-    while (parent.firstChild) {
-        parent.firstChild.remove();
-    }
+  while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
 }
 
 async function printMessage(string, printArea) {
-    let charArray = string.split("");
-    await asyncForEach(charArray, async (character) => {
-        let url = '';
-        switch (getCharacterType(character)) {
-            case 'letter':
-                url = `http://localhost:3000/letters/${character}`;
-                await fetch(url).then(handleErrors)
-                    .then(response => response.json())
-                    .then(response => {
-                        let newImg = createImg(response.url, character, character);
-                        printArea.append(newImg);
-                    }).catch(error => console.log(error.message));
-                break;
-            case 'number':
-                url = `http://localhost:3000/numbers/${character}`;
-                await fetch(url).then(handleErrors)
-                    .then(response => response.json())
-                    .then(response => {
-                        let newImg = createImg(response.url, character, character);
-                        printArea.append(newImg);
-                    }).catch(error => console.log(error.message));
-                break;
-            case 'space':
-                printArea.append(createSpace());
-                break;
-            // TO DO: handle symbols --for now we will just print a space
-            case 'symbol':
-                let symbolName = getSymbolName(character);
-                url = `http://localhost:3000/symbols/${symbolName}`
-                await fetch(url).then(handleErrors)
-                    .then(response => response.json())
-                    .then(response => {
-                        let newImg = createImg(response.url, character, character);
-                        printArea.append(newImg);
-                    }).catch(error => console.log(error.message));
-                break;
-            default:
-                console.log(`We were unable to process this character: ${character}`);
-        }
-    })
+  let charArray = string.split('');
+  await asyncForEach(charArray, async character => {
+    let url = '';
+    switch (getCharacterType(character)) {
+      case 'letter':
+        url = `/letters/${character}`;
+        await fetch(url)
+          .then(handleErrors)
+          .then(response => response.json())
+          .then(response => {
+            let newImg = createImg(response.url, character, character);
+            printArea.append(newImg);
+          })
+          .catch(error => console.log(error.message));
+        break;
+      case 'number':
+        url = `/numbers/${character}`;
+        await fetch(url)
+          .then(handleErrors)
+          .then(response => response.json())
+          .then(response => {
+            let newImg = createImg(response.url, character, character);
+            printArea.append(newImg);
+          })
+          .catch(error => console.log(error.message));
+        break;
+      case 'space':
+        printArea.append(createSpace());
+        break;
+      // TO DO: handle symbols --for now we will just print a space
+      case 'symbol':
+        let symbolName = getSymbolName(character);
+        url = `/symbols/${symbolName}`;
+        await fetch(url)
+          .then(handleErrors)
+          .then(response => response.json())
+          .then(response => {
+            let newImg = createImg(response.url, character, character);
+            printArea.append(newImg);
+          })
+          .catch(error => console.log(error.message));
+        break;
+      default:
+        console.log(`We were unable to process this character: ${character}`);
+    }
+  });
 }
 
 async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array)
-    }
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
 
 function handleErrors(response) {
-    if (!response.ok) {
-        throw Error(`Request failed with response ${response.status} : ${response.statusText}`);
-    }
-    return response;
+  if (!response.ok) {
+    throw Error(
+      `Request failed with response ${response.status} : ${response.statusText}`
+    );
+  }
+  return response;
 }
 
 function getCharacterType(character) {
-    if (lettersRegEx.test(character)) return 'letter';
-    if (numbersRegEx.test(character)) return 'number';
-    if (character === ' ') return 'space';
-    if (getSymbolName(character) !== 'symbol not found') return 'symbol';
+  if (lettersRegEx.test(character)) return 'letter';
+  if (numbersRegEx.test(character)) return 'number';
+  if (character === ' ') return 'space';
+  if (getSymbolName(character) !== 'symbol not found') return 'symbol';
 }
 
 function createImg(src, alt, title) {
-    var img = document.createElement('img');
-    img.className = 'letter';
-    img.src = src;
-    img.alt = alt;
-    img.title = title;
-    return img;
+  var img = document.createElement('img');
+  img.className = 'letter';
+  img.src = src;
+  img.alt = alt;
+  img.title = title;
+  return img;
 }
 
 function createSpace() {
-    var box = document.createElement('div');
-    box.className = 'space';
-    return box;
+  var box = document.createElement('div');
+  box.className = 'space';
+  return box;
 }
 
 function getSymbolName(character) {
-    switch (character) {
-        case '!':
-            return 'exclamation'
-        case '&':
-            return 'ampersand'
-        case '?':
-            return 'questionmark'
-        case '.':
-            return 'period'
-        case '#':
-            return 'hash'
-        case '%':
-            return 'percent'
-        case '+':
-            return 'plus'
-        case '=':
-            return 'equals'
-        case ',':
-            return 'comma'
-        case '\'':
-            return 'apostrophe'
-        case '\\/':
-            return 'slash'
-        case '\"':
-            return 'quote'
-        case '*':
-            return 'asterisk'
-        case '$':
-            return 'dollar'
-        default:
-            return 'symbol not found'
-    }
+  switch (character) {
+    case '!':
+      return 'exclamation';
+    case '&':
+      return 'ampersand';
+    case '?':
+      return 'questionmark';
+    case '.':
+      return 'period';
+    case '#':
+      return 'hash';
+    case '%':
+      return 'percent';
+    case '+':
+      return 'plus';
+    case '=':
+      return 'equals';
+    case ',':
+      return 'comma';
+    case "'":
+      return 'apostrophe';
+    case '\\/':
+      return 'slash';
+    case '"':
+      return 'quote';
+    case '*':
+      return 'asterisk';
+    case '$':
+      return 'dollar';
+    default:
+      return 'symbol not found';
+  }
 }
